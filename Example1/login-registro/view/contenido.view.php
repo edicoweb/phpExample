570d0de8709ecd8ca92f15c1598bd39ccf61287c
edicoweb <soyedicoweb@gmail.com>
2022-10-10 19:20:50 -0500

    login-register

---
+++ Example1/login-registro/view/contenido.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/contenido.css">
+    <title>Contenido</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Contenido del Sitio</h1>
+        <div class="logout-section"><a href="logout.php">Cerrar Sesion</a></div>
+        <article>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam, eos delectus! Iste adipisci magnam, cumque odit fuga odio magni laudantium sint quasi error quas nostrum ratione quo incidunt neque ut!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex veritatis totam, nihil amet esse qui ullam corporis eligendi aut saepe! Quam harum corrupti libero sint magni laborum autem et aliquam.</p>
+        </article>    </div>
+</body>
+</html>
---
+++ Example1/login-registro/view/registro.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/contenido.css">
+    <title>Registrate</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Registro</h1>
+        <form action="" method="post">
+            
+        </form>   
+    </div>
+</body>
+</html>
