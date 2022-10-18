41c1f03c805216f344b7ca34716b1da7d86eaaeb
edicoweb <soyedicoweb@gmail.com>
2022-10-17 19:26:22 -0500

    gallery

---
+++ Example1/Galeria/views/index.view.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Galeria</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Vianca Productos</h1>
+        </div>
+    </header>
+
+    <main>
+        <section class="fotos">
+            <div class="contenedor">
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/10.jpg" alt=""></a>
+                </div>
+                
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/11.jpg" alt=""></a>
+                </div>
+
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/12.jpg" alt=""></a>
+                </div>
+
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/13.jpg" alt=""></a>
+                </div>
+
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/14.jpg" alt=""></a>
+                </div>
+
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/15.jpg" alt=""></a>
+                </div>
+
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/16.jpg" alt=""></a>
+                </div>
+
+                <div class="thumb">
+                    <a href="#"><img src="assets/img-prueba/17.jpg" alt=""></a>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <div class="contenedor">
+            Derechos reservados @edicoweb 2022
+        </div>
+    </footer>
+</body>
+</html>
