9125ae070068804a6193a7c651ac333bd19f8859
edicoweb <soyedicoweb@gmail.com>
2022-11-08 19:21:41 -0500

    blog

--- Example1/blog-basico/views/index.view.php
+++ Example1/blog-basico/views/index.view.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilos.css">
+    <script src="https://kit.fontawesome.com/fe8f1122cf.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
@@ -41,45 +42,56 @@
             </div>
         </section>
 
-
         <section class="qh-section">
             <div class="qh-container section">
-
-
                 <div class="qh-container-skills">
                     <div class="skills-card">
-                        <img src="" alt="">
-                        <h1>Diseño de producto</h1>
-                        <p>La revolución tecnológica está cambiando de aspecto</p>
+                        <span class="icon-1"></span>
+                        <h1>Desarrollo Web</h1>
                     </div>
                     <div class="skills-card">
-                        <img src="" alt="">
-                        <h1>Diseño de logo</h1>
-                        <p>La revolución tecnológica está cambiando de aspecto</p>
+                        <span class="icon-2"></span>
+                        <h1>Diseño UI</h1>
                     </div>
                     <div class="skills-card">
-                    <img src="" alt="">
-                        <h1>Diseño de UI</h1>
-                        <p>La revolución tecnológica está cambiando de aspecto</p>
+                        <span class="icon-3"></span>
+                        <h1>Diseño Grafico</h1>
                     </div>
                     <div class="skills-card">
-                    <img src="" alt="">
-                        <h1>Diseño de Icons</h1>
-                        <p>La revolución tecnológica está cambiando de aspecto</p>
+                        <span class="icon-4"></span>
+                        <h1>Análisis de datos</h1>
                     </div>
                 </div>
-
                 <div class="qh-container-text">
                     <h1>¿Por qué contratarme para el próximo proyecto?</h1>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore porro reprehenderit magnam esse at, cupiditate iste nobis minus quidem distinctio iusto possimus temporibus, error perspiciatis, consequatur magni. Excepturi, id voluptatum.</p>
                     <a class="primary-button" href="">Mi Portafolio</a>
                 </div>
-
-
             </div>
         </section>
 
-
+        <section class="blog-section section">
+            <a href="">Ver mas posts <i class="fa-solid fa-arrow-right"></i></i></a>
+            <div class="blog-container">          
+                    <div class="blog-container-text">
+                        <h1>Lenovo Tech World</h1>
+                        <p>
+                            Lenovo Tech World es la reunión anual de la contracultura digital, aquellas comunidades que se presentan de una manera crítica de las tecnologías dentro de nuestros mecanismos de desarrollo de la sociedad.
+                            <a href=""><i class="fa-solid fa-arrow-right"></i></i> Seguir leendo</a>
+                        </p>
+                    </div>
+                    <div class="blog-container-img">
+                        <span>
+                        </span>
+                        <img src="assets/img/post.png" alt="Lenovo">
+                        
+                    </div>
+            </div>
+            <div class="pagination">
+                <a href="">Atras</a>
+                <a href="">Siguiente</a>
+            </div>
+        </section>
 
     </main>
 </body>
