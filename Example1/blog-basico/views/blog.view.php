1e1f2ad877f588161ad07d39e71b404f29dfcacb
edicoweb <soyedicoweb@gmail.com>
2022-11-13 19:37:08 -0500

    blog

---
+++ Example1/blog-basico/views/blog.view.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require 'head.php'; ?>
+<body>
+    <?php require 'header.view.php'; ?>
+    <main class="main-blog">
+        <section class="blog-section section" id="blog">
+            <div class="blog-container">
+                <div class="blog-container-img">
+                    <img src="assets/img/post.png" alt="Lenovo">
+                </div>        
+                <div class="blog-container-text">
+                    <h1>Lenovo Tech World</h1>
+                    <p>
+                        Lenovo Tech World es la reunión anual de la contracultura digital, aquellas comunidades que se presentan de una manera crítica de las tecnologías dentro de nuestros mecanismos de desarrollo de la sociedad.
+                        <a href=""><i class="fa-solid fa-arrow-right"></i></i> Seguir leendo</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="blog-section section" id="blog">
+            <div class="blog-container">
+                <div class="blog-container-img">
+                    <img src="assets/img/post.png" alt="Lenovo">
+                </div>        
+                <div class="blog-container-text">
+                    <h1>Lenovo Tech World</h1>
+                    <p>
+                        Lenovo Tech World es la reunión anual de la contracultura digital, aquellas comunidades que se presentan de una manera crítica de las tecnologías dentro de nuestros mecanismos de desarrollo de la sociedad.
+                        <a href=""><i class="fa-solid fa-arrow-right"></i></i> Seguir leendo</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <section class="blog-section section" id="blog">
+            <div class="blog-container">
+                <div class="blog-container-img">
+                    <img src="assets/img/post.png" alt="Lenovo">
+                </div>        
+                <div class="blog-container-text">
+                    <h1>Lenovo Tech World</h1>
+                    <p>
+                        Lenovo Tech World es la reunión anual de la contracultura digital, aquellas comunidades que se presentan de una manera crítica de las tecnologías dentro de nuestros mecanismos de desarrollo de la sociedad.
+                        <a href=""><i class="fa-solid fa-arrow-right"></i></i> Seguir leendo</a>
+                    </p>
+                </div>
+            </div>
+        </section>
+    </main>
+
+<?php require 'footer.view.php'; ?>
+</body>
+</html>
