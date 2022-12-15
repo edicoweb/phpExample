65258d319ac94ac32dfef5fe77ae0572ff0c5c4a
edicoweb <soyedicoweb@gmail.com>
2022-12-15 11:49:53 -0500

    blog

--- Example1/blog-basico/login.php
+++ Example1/blog-basico/login.php
@@ -1,3 +1,17 @@
 <?php
+    session_start();
+    require 'admin/config.php';
+    require 'function.php';
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $usuario = limpiarDatos($_POST['user']);
+        $password = limpiarDatos($_POST['password']);
+
+        if ($usuario == $blog_admi['usuario'] && $password == $blog_admi['password']) {
+            $_SESSION['admin'] = $blog_admi['usuario'];
+            header('Location:' . RUTA . '/admin');
+        }
+    }
+    
     require 'views/login.view.php';
 ?>
