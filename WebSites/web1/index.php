c22c8a7929861647b389f04a77ba2921a5a6c5cf
edicoweb <soyedicoweb@gmail.com>
2023-01-19 19:31:00 -0500

    web1

---
+++ WebSites/web1/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="assets/favicon/fav-icon.png" type="image/x-icon">
+    <title>vianca</title>
+</head>
+<body>
+    <header>
+        <div class="nav container">
+            <!-- Logo -->
+            <a href="#" class="logo">
+                <img src="./assets/img/logo-e.png" alt="">
+            </a>
+            <!-- Nav Icon -->
+            <div class="menu-icon">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <!-- Nav Lista -->
+            <ul class="navbar">
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#servicios">Servicios</a></li>
+                <li><a href="#nosotros">Nosotros</a></li>
+                <li><a href="#blog">Blog</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <!-- Home Section -->
+    <section class="home container" id="home">
+        <div class="home-text">
+            <h1><span>Creamos brillantes</span><br>soluciones tecnológicas</h1>
+            <p>Déjanos asesorarte y caminemos juntos en tu proceso de transformación digital.</p>
+            <a href="#" class="btn">Contáctenos</a>
+        </div>
+        <div class="home-img">
+            <img src="assets/img/portada.png" alt="">
+        </div>
+    </section>
+
+    <!-- Services section -->
+    <section class="services container" id="services">
+        <div class="heading">
+            <p>Nuestros Servicios</p>
+            <span></span>
+        </div>
+        <div class="services-content">
+
+            <!-- Box-1 -->
+            <a href="" class="box">
+                <img src="assets/icon/hand-trophy.svg" alt="">
+                <div class="box-text">
+                    <div class="title-services">
+                        <p>Diseño a Medida</p>
+                    </div>
+                </div>
+            </a>
+
+            <!-- Box-2 -->
+            <a href="" class="box">
+                <img src="assets/icon/palette-color.svg" alt="">
+                <div class="box-text">
+                    <div class="title-services">
+                        <p>Estrategias de Marketing Digital</p>
+                    </div>
+                </div>
+            </a>
+
+            <!-- Box-3 -->
+            <a href="" class="box">
+                <img src="assets/icon/rocket.svg" alt="">
+                <div class="box-text">
+                    <div class="title-services">
+                        <p>Diseño y Creatividad Publicitaria</p>
+                    </div>
+                </div>
+            </a>
+
+            <!-- Box-4 -->
+            <a href="" class="box">
+                <img src="assets/icon/squares-toys.svg" alt="">
+                <div class="box-text">
+                    <div class="title-services">
+                        <p>Posicionamiento Web SEO</p>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+    </section>
+    
+    <!-- Link To JS -->
+    <script src="js/main.js"></script>
+</body>
+</html>
