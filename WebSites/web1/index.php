7bfbcb159cb1e73ca54f06b317fef816e4500e49
edicoweb <soyedicoweb@gmail.com>
2023-01-20 19:38:11 -0500

    web1

--- WebSites/web1/index.php
+++ WebSites/web1/index.php
@@ -14,7 +14,7 @@
         <div class="nav container">
             <!-- Logo -->
             <a href="#" class="logo">
-                <img src="./assets/img/logo-e.png" alt="">
+                <img src="./assets/img/logo-e.svg" alt="">
             </a>
             <!-- Nav Icon -->
             <div class="menu-icon">
@@ -25,8 +25,8 @@
             <!-- Nav Lista -->
             <ul class="navbar">
                 <li><a href="#inicio">Inicio</a></li>
-                <li><a href="#servicios">Servicios</a></li>
-                <li><a href="#nosotros">Nosotros</a></li>
+                <li><a href="#services">Servicios</a></li>
+                <li><a href="#about">Nosotros</a></li>
                 <li><a href="#blog">Blog</a></li>
             </ul>
         </div>
@@ -51,50 +51,71 @@
             <span></span>
         </div>
         <div class="services-content">
-
             <!-- Box-1 -->
-            <a href="" class="box">
-                <img src="assets/icon/hand-trophy.svg" alt="">
+            <a href="" class="box box-1">
+                <img src="assets/icon/squares-toys.svg" alt="">
                 <div class="box-text">
                     <div class="title-services">
-                        <p>Diseño a Medida</p>
+                        <p>Software a Medida</p>
                     </div>
                 </div>
             </a>
-
             <!-- Box-2 -->
-            <a href="" class="box">
-                <img src="assets/icon/palette-color.svg" alt="">
+            <a href="" class="box box-2">
+                <img src="assets/icon/marketing.svg" alt="">
                 <div class="box-text">
                     <div class="title-services">
                         <p>Estrategias de Marketing Digital</p>
                     </div>
                 </div>
             </a>
-
             <!-- Box-3 -->
-            <a href="" class="box">
-                <img src="assets/icon/rocket.svg" alt="">
+            <a href="" class="box box-3">
+                <img src="assets/icon/palette-color.svg" alt="">
                 <div class="box-text">
                     <div class="title-services">
                         <p>Diseño y Creatividad Publicitaria</p>
                     </div>
                 </div>
             </a>
-
             <!-- Box-4 -->
-            <a href="" class="box">
-                <img src="assets/icon/squares-toys.svg" alt="">
+            <a href="" class="box box-4">
+                <img src="assets/icon/rocket.svg" alt="">
                 <div class="box-text">
                     <div class="title-services">
                         <p>Posicionamiento Web SEO</p>
                     </div>
                 </div>
             </a>
-
         </div>
     </section>
-    
+
+    <!-- About Section -->
+    <section class="about container" id="about">
+        <div class="about-img">
+            <img src="./assets/img/about-group.svg" alt="">
+        </div>
+        <div class="about-text">
+            <h2>¿Quiénes Somos?</h2>
+            <p>
+            Somos expertos en desarrollo de software, marketing digital y diseño creativo. Déjanos asesorarte y caminemos juntos en tu proceso de transformación digital.<br>
+            </p>
+        </div>
+    </section>
+
+    <!-- Newsletter section start -->
+    <section class="newsletter">
+        <div class="newsletter-content container">
+            <div class="newsletter-text">
+                <h2>Dejenos tu whatsapp</h2>
+                <p>Nosotros nos contactamos contigo.</p>
+            </div>
+            <form action="" method="get">
+                <input type="text" name="" id="" placeholder="Número de Celular" required>
+                <input type="submit" value="Enviar" class="btn">
+            </form>
+        </div>
+    </section>
     <!-- Link To JS -->
     <script src="js/main.js"></script>
 </body>
