236bbb94be3c4d070880af2a7dfeb7a061b4675a
edicoweb <soyedicoweb@gmail.com>
2023-02-20 17:46:15 -0500

    update

--- Example1/archivos/poo/Models/Task.php
+++ Example1/archivos/poo/Models/Task.php
@@ -8,13 +8,5 @@ class Task{
         $this->title = $title;
         $this->completed = $completed;
     }
-
-    public function save($nameFile){
-        $file = fopen($nameFile, 'w');
-        fwrite($file, "Titulo: {$this->title} \n Complete: " . ($this->completed ? 'SI' : 'NO'));
-        fclose($file);
-    }
 }
 
-$task = new Task('Ir al supermercado');
-$task->save("task-1.txt");
